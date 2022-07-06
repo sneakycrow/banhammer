ec53e7ae2046288ca54fffc9ddd783368b97ee0c
sneakycrow <zach@sneakycrow.dev>
2022-07-06 11:29:06 -0700

    integrate mantine

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import { ServerStyles, createStylesServer } from '@mantine/next';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+const stylesServer = createStylesServer();
+
+export default class _Document extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+
+        // Add your app specific logic here
+
+        return {
+            ...initialProps,
+            styles: [
+                initialProps.styles,
+                <ServerStyles html={initialProps.html} server={stylesServer} key="styles" />,
+            ],
+        };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head />
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        );
+    }
+
+}
