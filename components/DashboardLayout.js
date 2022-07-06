954e1232a7f9b07db07bfb33bf7a2ab556ebe822
sneakycrow <zach@sneakycrow.dev>
2022-07-06 15:40:00 -0700

    added drawer functionality to sidebar, wrapped index in dashboard

--- components/DashboardLayout.js
+++ components/DashboardLayout.js
@@ -1,21 +1,35 @@
 import Navigation from "./Navigation";
-import { Anchor, AppShell, Header, Navbar } from "@mantine/core";
-import Link from "next/link";
+import { AppShell, Button, Header, Navbar } from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
 
 export const DashboardLayout = ({ children }) => {
+  const [navbarOpened, setNavbarOpened] = useLocalStorage({
+    key: "navbar-state",
+    defaultValue: "opened",
+  });
   return (
     <AppShell
       padding="md"
       navbar={
-        <Navbar width={{ base: 300 }} height={500} p="xs">
-          <Link href={"/dashboard"}>
-            <Anchor component={"a"}>Dashboard</Anchor>
-          </Link>
+        <Navbar
+          width={{ base: navbarOpened === "opened" ? 300 : 75 }}
+          height="100vh"
+          p="xs"
+        >
+          <Navbar.Section>
+            <Button variant="subtle" fullWidth>
+              Assets/Hosts
+            </Button>
+          </Navbar.Section>
         </Navbar>
       }
       header={
         <Header>
-          <Navigation />
+          <Navigation
+            onMenuClick={() =>
+              setNavbarOpened(navbarOpened === "opened" ? "closed" : "opened")
+            }
+          />
         </Header>
       }
       styles={(theme) => ({
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import { ThemeIcon } from "@mantine/core";
+import { Hammer } from "tabler-icons-react";
+import DashboardLayout from "../components/DashboardLayout";
+
 export default function Home() {
-  return <h1>Waddup</h1>;
+  return (
+    <DashboardLayout>
+      <ThemeIcon size={400} radius={400} color="red">
+        <Hammer size={300} />
+      </ThemeIcon>
+    </DashboardLayout>
+  );
 }
