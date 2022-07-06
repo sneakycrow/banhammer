664ce2e800c6f120ca098beced28dcaffb61f712
sneakycrow <zach@sneakycrow.dev>
2022-07-06 13:52:32 -0700

    formatting

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,28 @@
-import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
 export default function App(props) {
   const { Component, pageProps } = props;
 
   return (
-      <>
-        <Head>
-          <title>banhammer.gg</title>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        </Head>
+    <>
+      <Head>
+        <title>banhammer.gg</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
 
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={{
-              colorScheme: 'light',
-            }}
-        >
-          <Component {...pageProps} />
-        </MantineProvider>
-      </>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,33 +1,36 @@
-import { ServerStyles, createStylesServer } from '@mantine/next';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyles, createStylesServer } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 const stylesServer = createStylesServer();
 
 export default class _Document extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx);
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
 
-        // Add your app specific logic here
+    // Add your app specific logic here
 
-        return {
-            ...initialProps,
-            styles: [
-                initialProps.styles,
-                <ServerStyles html={initialProps.html} server={stylesServer} key="styles" />,
-            ],
-        };
-    }
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles
+          html={initialProps.html}
+          server={stylesServer}
+          key="styles"
+        />,
+      ],
+    };
+  }
 
-    render() {
-        return (
-            <Html>
-                <Head />
-                <body>
-                <Main />
-                <NextScript />
-                </body>
-            </Html>
-        );
-    }
-
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
